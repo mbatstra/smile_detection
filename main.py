b7a723557472e5600a0766a4563b99760ea91f7f
mbatstra <mbatstra@student.codam.nl>
2023-04-25 16:38:39 +0200

    towards a more useful program

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import numpy as np
 import cv2
-from GuiClass import Gui
 from TimerBoolClass import Timer
+from datetime import datetime
+import time
 from matplotlib import pyplot as plt
 
 # load opencv classifiers
@@ -39,39 +40,89 @@ def scale_img(img, width, height):
     scaled_img = cv2.resize(img, None, fx=scale_x, fy=scale_y, interpolation=cv2.INTER_LINEAR)
     return scaled_img
 
-def main():
-    # compose gui
-    gui = Gui(800, 1200, (5, 197, 255))
-    button = cv2.imread('assets/buynow.png')
-    button = scale_img(button, 500, 100)
-    gui.addImage(button, 100, 600)
-    gui.addImage(button, 300, 600)
-    logo = cv2.imread('assets/euronet.png')
-    logo = scale_img(logo , 1000, 200)
-    gui.addImage(logo, 500, 100)
+def wait_startup():
+    # insert image made by iggy here
+    img = np.zeros((300, 400, 3), dtype=np.uint8)
+    img2 = cv2.imread('assets/strart.jpg')
+    img[150:150+img2.shape[0], 110:110+img2.shape[1]] = img2
+    while (True):
+        cv2.imshow("main", img)
+        if cv2.waitKey(1) & 0xFF == ord(' '):
+            break
 
-    # init timer
-    timer = Timer()
+# draw text to image returning offset for subsequent calls
+def draw_text(img, text, bg_on,
+          font=cv2.FONT_HERSHEY_PLAIN,
+          pos=(0, 0),
+          font_scale=3,
+          font_thickness=2,
+          text_color=(255, 255, 255),
+          text_color_bg=(0, 0, 0)
+          ):
+
+    x, y = pos
+    text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
+    text_w, text_h = text_size
+    if bg_on:
+        cv2.rectangle(img, pos, (x + text_w, y + text_h), text_color_bg, -1)
+    cv2.putText(img, text, (x, y + text_h + font_scale - 1), font, font_scale, text_color, font_thickness)
+
+    return text_size
+
+def countdown():
+    start = time.time()
+    while True:
+        now = time.time() - start
+        now_char = 3 - int(now)
+        ret, cam = vid.read()
+        draw_text(cam, str(now_char), True)
+        cam = scale_img(cam, 400, 300)
+        cv2.imshow("main", cam)
+        cv2.waitKey(1)
+        if now > 3:
+            break
+
+def record_withdrawal(timer):
     timer.start()
     while(True):
+        # get time to draw to frame
+        now = time.time() - timer.start_time
+        now_str = datetime.utcfromtimestamp(now).strftime('%M:%S.%f')[:-4]
+
         # scale frame and detect smile
         ret, cam = vid.read()
         ret, cam = smile_detec(cam)
+        draw_text(cam, now_str, True)
         cam = scale_img(cam, 400, 300)
 
         # set timer flag
         timer.set_true() if ret else timer.set_false()
     
         # display frame
-        gui.addImage(cam, 100, 100)
-        cv2.imshow('gui', gui.getImage())
-        
+        cv2.imshow("main", cam)
+
         # exit program
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            return
+
+
+def main():
+    # show starting screen
+    wait_startup()
+
+    # show countdown screen
+    countdown()
+
+    # init timer
+    timer = Timer()
+
+    # start withdrawal
+    record_withdrawal(timer)
+
     # print results
     print("time smiling :", timer.total_true_time)
-    print("time not smiling :", timer.total_false_time)
+    print("total time:", timer.get_total_time())
+
     # cleanup
     vid.release()
     cv2.destroyAllWindows()
